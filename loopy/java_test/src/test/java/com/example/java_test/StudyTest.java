9a6482b7e8fd745982babd403043c4c5568f7444
semi-cloud <sksmsrkdtpal@naver.com>
2022-04-12 15:53:39 +0900

    Junit5 Assertion

--- loopy/java_test/src/test/java/com/example/java_test/StudyTest.java
+++ loopy/java_test/src/test/java/com/example/java_test/StudyTest.java
@@ -2,11 +2,36 @@ package com.example.java_test;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class StudyTest {
     @Test
     @DisplayName("스터디 생성")
     void create() {
-        Study study = new Study();
+        Study study = new Study(10);
+
+        assertNotNull(study);
+        assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 DRAFT 여야 한다.");
+
+        //테스트가 깨지는 것을 한번에 알기
+        assertAll(
+                () ->assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                        () -> "스터디를 처음 만들면 상태값이 DRAFT 여야 한다.")
+
+        );
+
+        //예외 처리
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
+
+        //시간 설정
+        assertTimeout(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(20);
+        });
     }
 
     //테스트 시작 전 한번 실행
