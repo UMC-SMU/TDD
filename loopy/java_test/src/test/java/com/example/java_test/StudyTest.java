e25504d6f1f36b91baa2e5cee795c35c6669776f
semi-cloud <sksmsrkdtpal@naver.com>
2022-04-12 17:34:44 +0900

    JUnit5 테스트 반복

--- loopy/java_test/src/test/java/com/example/java_test/StudyTest.java
+++ loopy/java_test/src/test/java/com/example/java_test/StudyTest.java
@@ -2,6 +2,17 @@ package com.example.java_test;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 
@@ -59,8 +70,49 @@ class StudyTest {
         assumeTrue("LOCAL".equalsIgnoreCase(System.getenv("TEST_ENV")));
     }
 
+    //테스트 반복
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetition}")
+    void repeatTest(RepetitionInfo repetitionInfo){
+        System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" +
+                repetitionInfo.getTotalRepetitions());
+    }
 
-    //테스트 시작 전 한번 실행
+    //테스트 반복 : 단일 파라미터
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @ValueSource(ints = {10, 20, 30})
+    void parameterizedTestSingle(@ConvertWith(StudyConverter.class) Study study){
+        System.out.println(study.getLimit());
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter{   //하나의 인자에만 적용
+
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "can only convert to study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+    //테스트 반복 : 복수 파라미터
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+    void parameterizedTestMultiple(@AggregateWith(StudyAggregator.class) Study study){
+        System.out.println(study);
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        }
+    }
+
+
+   //테스트 시작 전 한번 실행
     @BeforeAll
     static void beforeAll(){
         System.out.println("before all the test");
